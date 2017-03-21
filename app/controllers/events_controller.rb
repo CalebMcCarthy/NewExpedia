129ca41df392f0697d99ebaf7a02a48ddc1fb006
Daniel Thompson <danthwa@gmail.com>
2017-03-21 15:35:12 -0700

    added API keys to project

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,22 @@
 class EventsController < ApplicationController
-  def index 
+
+    API_VERSION = 'v3'
+    API_KEY = '2qe8vhv5etpfagbcqsnq68sl4d'
+    SHARED_SECRET = 'c5th559a2n1uc'
+    TIMESTAMP = Time.now.to_i.to_s
+
+    PRODUCTION_ENDPOINT = 'api.ean.com/ean-services/rs/hotel/'
+    PRODUCTION_PORT = 80
+
+
+  def index
     @events = Event.all
   end
-  
+
   def show
     @event = Event.find params[:id]
   end
-  
+
   def new
   # render 'new'
     @assigned = session[:user_id]
@@ -27,11 +37,39 @@ class EventsController < ApplicationController
     @event[:name] = params[:name]
      #do this for each key in event
 
-    @event.save 
+    @event.save
 
   end
 
   def destroy
     Event.destroy params[:id]
+  end
+
+  def getHotels
+
+      hotelId = '201252'
+
+      otherElementsStr = '&cid=55505&minorRev=[x]&customerUserAgent=[xxx]&customerIpAddress=[xxx]&locale=en_US&currencyCode=USD'
+
+
+      sig = Digest::MD5.hexdigest( API_KEY + SHARED_SECRET + TIMESTAMP )
+
+      request_url = "/#{API_VERSION}/list}?apiKey=#{API_KEY} & sig=#{sig} & #{otherElementsStr} & hotelIdList=#{hotelId}"
+      req = Net::HTTP::Get.new(request_url)
+
+    #   puts "*******************"
+    #   print(PRODUCTION_ENDPOINT)
+
+      res = Net::HTTP.start( PRODUCTION_ENDPOINT, PRODUCTION_PORT, use_ssl: true ){
+
+          |http|
+
+          http.request(req)
+
+      }
+
+      print res
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do 
-  
+Rails.application.routes.draw do
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,28 +7,29 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-   root 'login#index' 
+   root 'login#index'
    get 'event/:id' => 'events#show'
-   get 'event/:id/edit' => 'events#edit' 
+   get 'event/:id/edit' => 'events#edit'
 
    post  'event' => 'events#create'
    get '/newevent' => 'events#new'
 
    get '/main' => 'events#index'
 
-   get '/users/:id' => 'users#show' 
+   get '/users/:id' => 'users#show'
    get 'users/:id/edit' => 'users#editprofile'
-   
+
    get 'register' => 'users#new'
    post 'users' => 'users#create'
 
    get 'login' => 'login#login'
-   post 'login' => 'login#received' 
+   post 'login' => 'login#received'
 
    get 'logout' => 'login#logout'
 
-  
-   
+   get 'gethotels' => 'events#getHotels'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
