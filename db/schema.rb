bb499d432cd9096c2a667d02d6702a95b907b058
130346 <130346@fwps365.org>
2017-03-22 16:17:07 -0700

    Added Map to main page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321005353) do
+ActiveRecord::Schema.define(version: 20170322225115) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170321005353) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id"
