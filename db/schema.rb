40b893d61cf2564c52168dd34e8f8d802283bfc0
130346 <130346@fwps365.org>
2017-03-25 11:16:16 -0700

    Added Latitude Longitude & Fixed New Events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323232200) do
+ActiveRecord::Schema.define(version: 20170325180959) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
